489e56e4f06953bf3a08bbd22d3820b103e5b4fe
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-05-16 19:55:02 +0900

    機能実装&不要ファイルの削除。

--- src/modules/project.rs
+++ src/modules/project.rs
@@ -1,1 +1,37 @@
+use std::fmt::{Display, Formatter, Result};
 
+use crate::utils::shell::question;
+
+enum ProjectTemplateType {
+    Default,
+}
+struct ProjectParams {
+    project_name: String,
+    project_template: ProjectTemplateType,
+}
+
+impl Display for ProjectParams {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let template = match self.project_template {
+            ProjectTemplateType::Default => "default",
+        };
+        write!(f, "Project: {}\nTemplate: {}", self.project_name, template)
+    }
+}
+
+fn create_empty_project(params: ProjectParams) {
+    println!("{}", params);
+}
+
+fn get_params_interactively() -> ProjectParams {
+    let project_name = question::kebab_loop("project name: ");
+    ProjectParams {
+        project_name,
+        project_template: ProjectTemplateType::Default,
+    }
+}
+
+pub fn create_project_interactively() {
+    let params = get_params_interactively();
+    create_empty_project(params);
+}
