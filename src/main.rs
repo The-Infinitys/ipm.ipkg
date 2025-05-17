691f347a5bb97cbb84188ccca5863477a89592a4
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-05-17 15:30:31 +0900

    ヘルプメッセージを出力する機能を実装完了。どんどん追加していく。

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ipkg::dprintln;
 use ipkg::modules::messages;
+use ipkg::utils::shell::args::{Argument, ArgumentType};
 use ipkg::utils::shell::{self, ExitStatus};
 fn main() -> Result<(), ExitStatus> {
     let command_data = shell::args::init();
@@ -10,10 +11,26 @@ fn main() -> Result<(), ExitStatus> {
         return Err(ExitStatus::NoArgs);
     }
     let command = &args[0];
-    match command {
-        "--help" => messages::help("general"),
-        "--version" => messages::version(),
-        _ => messages::error("unknown_command"),
+    let sub_args: Vec<&Argument> = args[1..].iter().collect();
+    enum SubCommand {
+        Help,
+        Version,
+        Unknown,
+    }
+    let arg_str = command.arg_str.as_str();
+    let sub_command: SubCommand = match &command.arg_type {
+        ArgumentType::LongOpt => match arg_str {
+            "--help" => SubCommand::Help,
+            "--version" => SubCommand::Version,
+            _ => SubCommand::Unknown,
+        },
+        ArgumentType::ShortOpt => SubCommand::Unknown,
+        ArgumentType::Simple => SubCommand::Unknown,
+    };
+    match sub_command {
+        SubCommand::Help => messages::help(sub_args),
+        SubCommand::Version => messages::version(),
+        SubCommand::Unknown => messages::unknown(),
     }
     Ok(())
 }
--- src/modules/messages.rs
+++ src/modules/messages.rs
@@ -1,3 +1,5 @@
+use crate::utils::shell::args::Argument;
+
 struct CargoPackageInfo {
     name: &'static str,
     version: &'static str,
@@ -11,22 +13,56 @@ fn get_info() -> CargoPackageInfo {
     }
 }
 
-pub fn welcome() {
-    let welcome_str = include_str!("./welcome/welcome.txt");
+fn insert_info(text: &'static str) -> String {
     let cargo_package = get_info();
     let replace_list = vec![
         ["name", cargo_package.name],
         ["version", cargo_package.version],
         ["architecture", cargo_package.architecture],
     ];
-    let mut welcome_str = welcome_str.to_string();
+    let mut text = text.to_string();
     for replaces in replace_list {
-        welcome_str = welcome_str.replace(format!("{{{}}}", replaces[0]).as_str(), replaces[1]);
+        text = text.replace(format!("{{{}}}", replaces[0]).as_str(), replaces[1]);
     }
+    text
+}
+
+pub fn welcome() {
+    let welcome_str = insert_info(include_str!("./messages/welcome.txt"));
     println!("{}", welcome_str);
 }
 
 pub fn version() {
     let cargo_package = get_info();
     println!("{} {}", cargo_package.name, cargo_package.version);
+}
+
+pub fn help(args: Vec<&Argument>) {
+    let help_type: HelpType = if args.is_empty() {
+        HelpType::General
+    } else {
+        match args[0].arg_str.as_str() {
+            "install" => HelpType::Install,
+            _ => HelpType::General,
+        }
+    };
+    show_help(help_type);
+}
+
+enum HelpType {
+    General,
+    Install,
+}
+fn get_help_msg(help_type: HelpType) -> String {
+    insert_info(match help_type {
+        HelpType::General => include_str!("./messages/help/general.txt"),
+        HelpType::Install => include_str!("./messages/help/install.txt"),
+    })
+}
+fn show_help(help_type: HelpType) {
+    let help_msg = get_help_msg(help_type);
+    println!("{}", help_msg);
+}
+pub fn unknown() {
+    eprintln!("unknown help type");
 }
