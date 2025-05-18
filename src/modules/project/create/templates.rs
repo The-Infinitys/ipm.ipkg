87e13bccfd9a8100d88b1f7b418992ec8fd2b4d8
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-18 20:32:33 +0900

    asdf

--- src/modules/project/create/templates.rs
+++ src/modules/project/create/templates.rs
@@ -14,9 +14,23 @@ fn setup(setup_list: Vec<SetUpList>) -> Result<(), Error> {
 }
 
 pub fn default() -> Result<(), Error> {
-    let setup_list = vec![SetUpList {
-        path: String::from("src/main.sh"),
-        content: include_str!("templates/default/src/main.sh").to_string(),
-    }];
+    let setup_list = vec![
+        SetUpList {
+            path: String::from("src/main.sh"),
+            content: include_str!("templates/default/src/main.sh").to_string(),
+        },
+        SetUpList {
+            path: String::from("scripts/install.sh"),
+            content: include_str!("templates/default/scripts/install.sh").to_string(),
+        },
+        SetUpList {
+            path: String::from("scripts/remove.sh"),
+            content: include_str!("templates/default/scripts/remove.sh").to_string(),
+        },
+        SetUpList {
+            path: String::from("scripts/purge.sh"),
+            content: include_str!("templates/default/scripts/purge.sh").to_string(),
+        },
+    ];
     setup(setup_list)
 }
