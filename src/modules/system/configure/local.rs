08cac6e0a1910d85cb481f5b7fd64b2505b1cce2
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-05-21 05:16:19 +0000

    system configureの仮実装が完了。

--- src/modules/system/configure/local.rs
+++ src/modules/system/configure/local.rs
@@ -1,10 +1,29 @@
 use crate::utils::files::file_creation;
 use std::env;
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 use std::path::Path;
 pub fn configure() -> Result<(), Error> {
+    let configure_list = [
+        [".ipkg/README.md", include_str!("data/local/README.md")],
+        [".ipkg/bin/ipkg-local", "data/local/ipkg-local"],
+    ];
     let home_dir = env::var("HOME").unwrap();
     let home_dir = Path::new(&home_dir);
-    file_creation(home_dir.join("").to_str().unwrap(), "")?;
+    for configure_data in configure_list {
+        let creation_result = file_creation(
+            home_dir.join(configure_data[0]).to_str().unwrap(),
+            configure_data[1],
+        );
+        match creation_result {
+            Ok(()) => {
+                continue;
+            }
+            Err(e) => {
+                if e.kind() != ErrorKind::AlreadyExists {
+                    return Err(e);
+                }
+            }
+        }
+    }
     Ok(())
 }
